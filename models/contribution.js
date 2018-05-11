e1024076df081b61601f3a9f2a59777e8a8ae955
puntje <fc.puntje@gmail.com>
2018-05-11 17:38:46 +0200

    Closes #12 - duplicate key issue on bulk distribution (same hash)

--- models/contribution.js
+++ models/contribution.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const ContributionSchema = new mongoose.Schema({
   hash: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   blockNumber: {
     type: Number,
