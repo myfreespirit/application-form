f93f63b93cc91ef0df3ae4dae22c61ecc6ea4043
puntje <fc.puntje@gmail.com>
2018-05-21 02:41:10 +0200

    Closes #13 - new block height added for latest price change

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -20,6 +20,7 @@ export class DataService {
   ];
   airdropAmount = Math.pow(10, 7);
   distributionRates = [
+    { block: 5539570, value: this.airdropAmount / 4 },
     { block: 4866696, value: this.airdropAmount / 2 },
     { block: 0, value: this.airdropAmount }
   ];
