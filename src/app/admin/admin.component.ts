57e65fb0c9cbdb921f622d0a0e2b8a6f8ba9acac
puntje <fc.puntje@gmail.com>
2019-02-09 19:26:09 +0100

    Basic admin panel

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Inject } from '@angular/core';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 import { AuthService } from '../services/auth.service';
@@ -13,35 +13,44 @@ import { TestnetService } from '../services/testnet.service';
   templateUrl: './admin.component.html',
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('collapsed, void', style({ height: '0px', minHeight: '0', display: 'none' })),
       state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
+      transition('expanded <=> collapsed', animate('125ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> void', animate('125ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+    ])
   ]
 })
 
 
 export class AdminComponent implements OnInit, AfterViewInit {
 
-  displayedColumns = ['username', 'telegram'];
+  data = [];
+  displayedTabs = ['BACKLOG', 'RESETS', 'OBSERVERS', 'TESTERS', 'REJECTED'];
+  displayedColumns = ['telegram', 'username', 'Total EXRN', 'Total EXRT'];
   expandedElement: TestnetRegistrationElement;
-  dataSource: MatTableDataSource<TestnetRegistrationElement>;
+  dataSource: MatTableDataSource<TestnetRegistrationElement>[];
 
-  @ViewChild('paginator') paginator: MatPaginator;
-  @ViewChild('sort') sort: MatSort;
+  @ViewChildren(MatPaginator) paginator = new QueryList<MatPaginator>();
+  @ViewChildren(MatSort) sort = new QueryList<MatSort>();
 
   constructor(@Inject(AuthService) public auth: AuthService, @Inject(TestnetService) public testnet: TestnetService) {
 	auth.handleAuthentication();
 
-	this.dataSource = new MatTableDataSource([]);
-	this.getAllRegistrations();
+	this.dataSource = [];
+	this.displayedTabs.forEach((tab, index) => {
+		this.dataSource[tab] = new MatTableDataSource([]);
+		this.dataSource[tab].paginator = this.paginator.toArray()[index];
+		this.dataSource[tab].sort = this.sort.toArray()[index];
+		console.log(tab);
+	});
   }
 
 
   ngAfterViewInit() {
-	this.dataSource.paginator = this.paginator;
-
-	this.dataSource.sort = this.sort;
+	this.displayedTabs.forEach((tab, index) => {
+		this.dataSource[tab].paginator = this.paginator.toArray()[index];
+		this.dataSource[tab].sort = this.sort.toArray()[index];
+	});
   }
 
 
@@ -52,33 +61,65 @@ export class AdminComponent implements OnInit, AfterViewInit {
   }
 
 
-  applyFilter(filterValue: string) {
+  applyFilter(filterValue: string, tab: string) {
 	filterValue = filterValue.trim();
 	filterValue = filterValue.toLowerCase();
-	this.dataSource.filter = filterValue;
+	this.dataSource[tab].filter = filterValue;
 
-	if (this.dataSource.paginator) {
-		this.dataSource.paginator.firstPage();
+	if (this.dataSource[tab].paginator) {
+		this.dataSource[tab].paginator.firstPage();
 	}
   }
 
 
   private getAllRegistrations() {
+	this.data = [];
+	this.displayedTabs.forEach(tab => {
+		console.log("test");
+		this.data[tab] = [];
+	});
+
   	this.testnet.getAllRegistrations().subscribe((result: any[]) => {
-		const data = [];
 		result.forEach(el => {
-			data.push({ 'username': el.username, 'telegram': el.telegram });
+			let category = "";
+			const lastStatus = el.states[el.states.length - 1].status;
+			switch (lastStatus) {
+				case 'APPLIED':
+					category = 'BACKLOG';
+					break;
+				case 'RESET REQUEST':
+					category = 'RESETS';
+					break;
+				case 'APPROVED OBSERVER':
+					category = 'OBSERVERS';
+					break;
+				case 'APPROVED TESTER':
+					category = 'TESTERS';
+					break;
+				case 'REJECTED':
+					category = 'REJECTED';
+					break;
+			}
+
+			this.data[category].push({ 'telegram': '@'+el.telegram, 'username': el.username, 'access_key': el.hash, 'wallet': el.wallet, 'motivation': el.motivation, 'states': el.states });
 		});
 
-		this.dataSource = new MatTableDataSource(data);
-		this.dataSource.paginator = this.paginator;
-		this.dataSource.sort = this.sort;
+		this.displayedTabs.forEach((tab, index) => {
+			this.dataSource[tab] = new MatTableDataSource(this.data[tab]);
+			this.dataSource[tab].paginator = this.paginator.toArray()[index];
+			this.dataSource[tab].sort = this.sort.toArray()[index];
+		});
 	});
   }
 }
 
 
 export interface TestnetRegistrationElement {
-  username: string;
   telegram: string;
+  username: string;
+  access_key: string;
+  wallet: string;
+  motivation: string;
+  states: any;
+  expanded: boolean;
 }
